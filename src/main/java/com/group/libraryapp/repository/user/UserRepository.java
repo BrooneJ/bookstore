053ba4895f6c0a15c690d96040a930c2c2d6b819
HyungwonJin <hwjin92@gmail.com>
2024-12-08 12:30:08 +0900

    refactor: move files

--- src/main/java/com/group/libraryapp/repository/user/UserRepository.java
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -1,4 +1,4 @@
-package com.group.libraryapp.repository;
+package com.group.libraryapp.repository.user;
 
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
