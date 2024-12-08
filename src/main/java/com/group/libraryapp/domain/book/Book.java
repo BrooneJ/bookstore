c1ee4d8063f1fdf6bf4f643130fad23f2dd2b720
HyungwonJin <hwjin92@gmail.com>
2024-12-08 16:50:24 +0900

    feat: implement book functionality

---
+++ src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -0,0 +1,26 @@
+package com.group.libraryapp.domain.book;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    @Column(nullable = false)
+    private String name;
+
+    protected Book() {
+    }
+
+    public Book(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(String.format("error: name(%s) is not exist", name));
+        }
+        this.name = name;
+    }
+}
